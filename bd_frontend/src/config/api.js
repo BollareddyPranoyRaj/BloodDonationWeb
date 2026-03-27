c5a8bc3f58cadc18dc5fecb17921a2b7e678ee81
BollareddyPranoyRaj1 <bollareddypranoyraj@gmail.com>
2026-03-27 15:58:45 +0530

    Read frontend API server URL from environment

--- bd_frontend/src/config/api.js
+++ bd_frontend/src/config/api.js
@@ -1,2 +1,3 @@
-export const SERVER_BASE_URL = 'http://localhost:5001';
+export const SERVER_BASE_URL =
+  process.env.REACT_APP_SERVER_BASE_URL || 'http://localhost:5001';
 export const API_BASE_URL = `${SERVER_BASE_URL}/api`;
